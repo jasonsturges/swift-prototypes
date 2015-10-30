500cffa0c51a0864a6be02aa4a24b1c104546cf7
Jason Sturges <jason@jasonsturges.com>
2015-10-30 11:29:48 -0500

    XMLUsingSWXMLHash prototype

--- XMLUsingSWXMLHash/XMLUsingSWXMLHash/ViewController.swift
+++ XMLUsingSWXMLHash/XMLUsingSWXMLHash/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import SWXMLHash
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("data", ofType: "xml")
+        let data = NSData(contentsOfFile: path!)
+        let xml = SWXMLHash.parse(data!)
+        
+        print(xml["root"]["catalog"]["book"][1].element?.attributes["id"])
+        
+        for elem in xml["root"]["catalog"]["book"] {
+            print(elem["genre"].element!.text!)
+        }
+        
+        do {
+            try print(xml["root"]["catalog"]["book"].withAttr("id", "123")["author"].element!.text!)
+        } catch let error as XMLIndexer.Error {
+            // error is an XMLIndexer.Error instance that you can deal with
+            print("An XMLIndexer.Error occurred: \(error.description)")
+        } catch {
+            print("An unknown error occurred")
+        }
+
     }
 
     override func didReceiveMemoryWarning() {

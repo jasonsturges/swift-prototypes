3fabf518e7ce8664c84f5c627d022af0542d56ab
Jason Sturges <jason@jasonsturges.com>
2016-07-08 11:45:25 -0500

    Drag and drop project template

---
+++ DragAndDrop/DragAndDrop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DragAndDrop
+//
+//  Created by Jason Sturges on 7/8/16.
+//  Copyright © 2016 Jason Sturges. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DragAndDrop/DragAndDrop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DragAndDrop
+//
+//  Created by Jason Sturges on 7/8/16.
+//  Copyright © 2016 Jason Sturges. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

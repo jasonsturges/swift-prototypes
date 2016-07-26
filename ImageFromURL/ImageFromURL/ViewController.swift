74bf5db5e02d0453af5d89a146328497fb4063e6
Jason Sturges <jason@jasonsturges.com>
2016-07-26 15:20:43 -0500

    Image from URL prototype

--- ImageFromURL/ImageFromURL/ViewController.swift
+++ ImageFromURL/ImageFromURL/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let imageUrl:NSURL = NSURL(string: "http://labs.jasonsturges.com/swift/assets/checkerboard.png")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            let data = NSData(contentsOfURL: self.imageUrl)
+            dispatch_async(dispatch_get_main_queue(), {
+                self.imageView.image = UIImage(data: data!)
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 

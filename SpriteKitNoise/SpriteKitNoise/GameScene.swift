f1a550daaa48eeb1dad78e52d48dea87a7d58824
Jason Sturges <jason@jasonsturges.com>
2017-01-04 11:05:27 -0600

    Migrate to Swift 3

--- SpriteKitNoise/SpriteKitNoise/GameScene.swift
+++ SpriteKitNoise/SpriteKitNoise/GameScene.swift
@@ -13,7 +13,7 @@ class GameScene: SKScene {
     var noiseTexture:SKTexture!
     var noiseSprite:SKSpriteNode!
     
-    override func didMoveToView(view: SKView) {
+    override func didMove(to view: SKView) {
         noiseTexture = SKTexture(noiseWithSmoothness: 1.1, size: CGSize(width: 1024, height: 768), grayscale: false)
         //noiseTexture = SKTexture(vectorNoiseWithSmoothness: 1.1, size: CGSize(width: 1024, height: 768))
         noiseSprite = SKSpriteNode(texture: noiseTexture)

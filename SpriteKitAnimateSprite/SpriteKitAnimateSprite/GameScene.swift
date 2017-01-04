0b597b50702eff424b1eff9cff573c8be9082d8b
Jason Sturges <jason@jasonsturges.com>
2017-01-04 11:03:51 -0600

    Migrate to Swift 3

--- SpriteKitAnimateSprite/SpriteKitAnimateSprite/GameScene.swift
+++ SpriteKitAnimateSprite/SpriteKitAnimateSprite/GameScene.swift
@@ -12,8 +12,8 @@ class GameScene: SKScene {
     
     var sprite:SKSpriteNode!
     
-    override func didMoveToView(view: SKView) {
-        sprite = SKSpriteNode(color: UIColor.orangeColor(), size: CGSize(width: 64, height: 64))
+    override func didMove(to view: SKView) {
+        sprite = SKSpriteNode(color: UIColor.orange, size: CGSize(width: 64, height: 64))
         sprite.position = CGPoint(x: 512, y: 384)
         
         self.addChild(sprite);
@@ -22,21 +22,21 @@ class GameScene: SKScene {
     }
     
     func animate() {
-        let action1:SKAction = SKAction.scaleTo(4, duration: 2.5)
-        sprite.runAction(action1, completion: {
-            let action2:SKAction = SKAction.scaleTo(1, duration: 2.5)
-            self.sprite.runAction(action2)
+        let action1:SKAction = SKAction.scale(to: 4, duration: 2.5)
+        sprite.run(action1, completion: {
+            let action2:SKAction = SKAction.scale(to: 1, duration: 2.5)
+            self.sprite.run(action2)
         })
         
-        let action3:SKAction = SKAction.rotateByAngle(6.28319, duration: 5)
-        sprite.runAction(action3)
+        let action3:SKAction = SKAction.rotate(byAngle: 6.28319, duration: 5)
+        sprite.run(action3)
         
-        let action4:SKAction = SKAction.colorizeWithColor(UIColor.yellowColor(), colorBlendFactor: 1, duration: 2.5)
-        let action5:SKAction = SKAction.colorizeWithColor(UIColor.orangeColor(), colorBlendFactor: 1, duration: 2.5)
+        let action4:SKAction = SKAction.colorize(with: UIColor.yellow, colorBlendFactor: 1, duration: 2.5)
+        let action5:SKAction = SKAction.colorize(with: UIColor.orange, colorBlendFactor: 1, duration: 2.5)
         let sequence = SKAction.sequence([action4, action5])
-        sprite.runAction(sequence) {() -> Void in
+        sprite.run(sequence, completion: {() -> Void in
             self.animate()
-        }
+        }) 
     }
     
 }

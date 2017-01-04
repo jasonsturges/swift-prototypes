5d28f78f4fcad620038d335238c9403a8ec1c957
Jason Sturges <jason@jasonsturges.com>
2017-01-04 12:46:13 -0600

    MustacheTemplate project scaffolding

---
+++ MustacheTemplate/MustacheTemplate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MustacheTemplate
+//
+//  Created by Jason Sturges on 1/4/17.
+//  Copyright © 2017 Jason Sturges. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MustacheTemplate/MustacheTemplate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MustacheTemplate
+//
+//  Created by Jason Sturges on 1/4/17.
+//  Copyright © 2017 Jason Sturges. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
